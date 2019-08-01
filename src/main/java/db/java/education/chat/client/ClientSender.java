3f9beba9ed0e0bb0ab58f62ce94673cd307e793d
KatarinaAet <Katrin.Kulagina97@yandex.ru>
2019-08-01 12:34:48 +0300

    client add thread start

--- src/main/java/db/java/education/chat/client/ClientSender.java
+++ src/main/java/db/java/education/chat/client/ClientSender.java
@@ -5,28 +5,28 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Date;
 
-public class Client {
+public class ClientSender {
     public static void main(String[] args) {
         try (final Socket server = new Socket("localhost", 666)) {
 
             //region read
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try (final BufferedReader in =
-                                 new BufferedReader(
-                                         new InputStreamReader(
-                                                 new BufferedInputStream(
-                                                         server.getInputStream(), 100)))) {
-                        while (!Thread.interrupted()) {
-                            System.out.println(in.readLine());
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+            Thread thread = new Thread(() -> {
+                try (final BufferedReader in =
+                             new BufferedReader(
+                                     new InputStreamReader(
+                                             new BufferedInputStream(
+                                                     server.getInputStream(), 100)))) {
 
+                    while (!Thread.interrupted()) {
+                        System.out.println(in.readLine());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+
             });
+            thread.setDaemon(true);
+            thread.start();
             //endregion
 
             //region write
@@ -45,14 +45,14 @@ public class Client {
                     while (true) {
                         String putLine = console.readLine();
 
-                        switch (putLine.substring(0,putLine.indexOf(" "))){
+                        switch (putLine.substring(0, putLine.indexOf(" "))) {
                             case "/snd":
                                 out.write("/snd " + new Date().toString() + putLine.replace("/snd", " "));
                                 out.newLine();
                                 out.flush();
                                 break;
-                            case "/hist":
-                                out.write("/hist");
+                            default:
+                                out.write(putLine);
                                 break;
                         }
 
